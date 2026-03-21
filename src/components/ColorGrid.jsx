92df8d5dcc0e9d2237963e589b59d383148fca6b
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-03-21 01:02:08 +0100

    feat: improve ColorGrid and page layout

--- src/components/ColorGrid.jsx
+++ src/components/ColorGrid.jsx
@@ -59,7 +59,7 @@ function ColorGrid({ colors }) {
         <div
           className={
             layout === "grid"
-              ? "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4"
+              ? "auto-grid"
               : "flex flex-col gap-2"
           }
         >
